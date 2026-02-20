c8c629c71d2b5782b7012b46d84b01be369aede9
cuds9896 <jackcudley@gmail.com>
2026-02-20 19:05:46 +0000

    fix app name (oops) and create table for documents with suitable tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,9 +2,23 @@ import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-describe("Document Viewer Application", () => {
-  it("renders the title without crashing", () => {
+describe("App", () => {
+  it("renders the title", () => {
     render(<App />);
     expect(screen.getByText("BrightHR tech task")).toBeInTheDocument();
+  });
+
+  it("renders the table headers", () => {
+    render(<App />);
+    expect(screen.getByText("Name")).toBeInTheDocument();
+    expect(screen.getByText("Type")).toBeInTheDocument();
+    expect(screen.getByText("Added")).toBeInTheDocument();
+  });
+
+  it("renders table rows", () => {
+    render(<App />);
+    expect(screen.getByText("Cost centres")).toBeInTheDocument();
+    expect(screen.getByText("csv")).toBeInTheDocument();
+    expect(screen.getByText("2016-08-12")).toBeInTheDocument();
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
+import { mockData } from "./constants/mockData";
+
 function App() {
   return (
-    <>
-      <h1>BrightHR tech task</h1>
-    </>
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">BrightHR tech task</h1>
+      <table className="w-full border-collapse rounded-2xl overflow-hidden">
+        <thead>
+          <tr className="flex flex-row bg-gray-200 items-center justify-between gap-4 p-2 border-b">
+            <th className="w-48 p-2 text-left">Name</th>
+            <th className="w-48 p-2 text-left">Type</th>
+            <th className="w-48 p-2 text-left">Added</th>
+          </tr>
+        </thead>
+        <tbody>
+          {mockData.map((item, index) => {
+            return (
+              <tr
+                className={`flex flex-row bg-gray-200 items-center justify-between gap-4 p-2 border-b border-gray-400 last:border-0 ${item.type === "folder" ? "underline" : ""}`}
+                key={index}
+              >
+                <td className="w-48 px-2">{item.name}</td>
+                <td className="w-48 px-2">{item.type}</td>
+                <td className="w-48 px-2">{item.added}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
